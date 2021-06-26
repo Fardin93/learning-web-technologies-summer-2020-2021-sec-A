1c446a41697590e143426d14b88d01abc5c9da20
Fardin Farhan <84652394+Fardin93@users.noreply.github.com>
2021-06-26 14:29:18 +0600

    done

---
+++ Lab_Task_5_php/Page2.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+<form action="" method="POST">
+<table>
+<tr>
+    <td>
+    First name: 
+    <br>
+        <input type="text" name="university"><br>
+        </td>
+</tr>
+    <tr>
+        <td>
+       Department: 
+        <br>
+        <input type="text" name="Department"  ><br>
+        </td>
+    </tr>
+    <tr>
+        <td>
+             CGPA:
+        <br>
+        <input type="text" name="CGPA"  >    
+        </td>
+    </tr>
+    <tr>
+        <td>
+        Year Complete: 
+                
+       <br>
+        <input type="text" name="yearComplete"  >        
+        </td>
+    
+    </tr>
+    <tr>
+            <td>
+                Company name:
+            
+
+        <br>   
+            <input type="text" name="company name " >       
+            </td>
+    
+    </tr>
+
+    <tr>
+        <td>
+            Years of Experience:
+       <br>
+        <input type="text" name="years of experience" ><br>     
+        <a href="file:///D:/Study/Web%20Tech/Lab%20Task%202/zaku3.html">NEXT</a>
+        </td>
+    
+    </tr>
+    
+    
+    </form>
+
+    </table>
+</body>
+</html>
---
+++ Lab_Task_5_php/page3.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ "
+    <title>page 3</title>
+</head>
+<body>
+    <h1>Your all details</h1>
+    <a href="file:///D:/Study/Web%20Tech/Lab%20Task%202/zaku1.html">Back to home </a>
+</body>
+</html>
